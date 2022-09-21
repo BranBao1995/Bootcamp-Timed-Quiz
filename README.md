55745dbf20829a24d873a7e26a5251190bf0cc45
BranBao1995 <brandon.0702@hotmail.com>
2022-09-20 21:54:01 -0400

    added the 'view scores' feature and added some more styling. Updated REAMME.md

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# Bootcamp-Timed-Quiz
-Bootcamp module 4 challenge of making a timed quiz with javascript.
+# Timed-Quiz
+
+Module 4 challenge of making a timed quiz with javascript.
+
+## Description
+
+- The quiz is made to render different JavaScript related questions to test a user's knowledge.
+
+- More questions with various degrees of difficulties will be added in the future.
+
+* The user will be greeted with a welcome page when first launching the application.
+
+* Once the user hits the 'start game' button, the first question will be rendered and the time starts counting down.
+
+* For now, the timer is set to be 50 seconds.
+
+* Five seconds will be deducted from the timer each time a user answers a question wrong.
+
+* 2 points will be added to the score if a right choice is made and 1 point will be taken away if a wrong choice is made.
+
+* After the user makes a selection for his/her answer, a message will pop up to indicate whether or not the user got the correct answer.
+
+* The game is over when time is up, or when the user finishes all question. The user will then be taken to a page which shows the final score for this round of game, as well as an input field where the user may enter his/her initials.
+
+* The user can store his/her score and initials to the high scores list by clicking on the 'submit' button.
+
+* The user will be taken to the high scores page where a list of history of scores and names will be displayed. The user may also visit these information before the start of the game by clicking 'view high scores' on the top left corner. Note that once the game is started, the high scores page will be unavailable.
+
+* The user may choose to go back to the welcome page by clicking on the 'go back' button in the high scores page. The user may also erase all scores history by clicking 'clear scores'.
+
+## Usage
+
+- When the application is launched, the user will see this:
+
+  ![JavaScript quiz welcome page](./assets/images/welcome-page.JPG)
+
+  click the 'start game' button to begin. Users can also click 'view high scores' to review scores history.
+
+- After clicking the "Start Game" button, the game stars and the first question will appear:
+
+  ![In-game screenshot](./assets/images/during-game.JPG)
+
+- After making a selection, a message will pop up telling the user their choice is correct/incorrect:
+
+  ![Message pop up telling the user the result](./assets/images/show-result-message.JPG)
+
+- After the time is up, or the user finishes all questions, it will jump to the final score page:
+
+  ![Page showing final score](./assets/images/final-score-page.JPG)
+
+  Once the submit button is clicked, the score and the initials will be stored and displayed in the view high scores page.
+
+- This is what the high scores page looks like:
+
+  ![View high scores page](./assets/images/view-high-scores-page.JPG)
+
+  The user may click on 'go back' to restart the game or click on 'clear scores' to clear all history.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
+// declaring all HTML elements that need to be manupulated as variables
+
 const timeDisplay = document.querySelector(".time-display");
+const scoreCounter = document.querySelector(".score-counter");
 const scoreDisplay = document.querySelector(".score-display");
 const resultMessage = document.querySelector(".message");
 
@@ -11,48 +14,67 @@ const startButton = document.querySelector(".start-button");
 const submitScoreButton = document.querySelector(".submit-score");
 const viewHighScoresButton = document.querySelector(".view-scores");
 const backToWelcomePageButton = document.querySelector(".button-go-back");
+const clearScoresButton = document.querySelector(".clear-scores");
 
 const questionArray = document.querySelectorAll(".quiz-question");
 
 const nameInput = document.querySelector("#initials");
 
-console.log(buttonArray);
+let score = 0; // initialize the score variable
+let timer = 50; // initialize the timer variable
 
-let score = 0;
-let timer = 10;
-
+// initialize the score and initials tracking object
 let scoreListItem = {
   scoreArray: [],
   userInitials: [],
 };
 
-scoreListItem = JSON.parse(localStorage.getItem("scoreListItem"));
+// if local storage is not empty, fill the object with infomation at the launch of the application
+if (localStorage.getItem("scoreListItem") != null) {
+  scoreListItem = JSON.parse(localStorage.getItem("scoreListItem"));
+  // otherwise set object to be empty
+} else {
+  scoreListItem = {
+    scoreArray: [],
+    userInitials: [],
+  };
+}
 
+// init function triggered when the 'start game' button is clicked
 function init() {
+  // enable all UI buttons for all questions
   enableButtons();
-  viewHighScoresButton.style.display = "none";
-  timeDisplay.textContent = "Time: " + timer;
+  viewHighScoresButton.style.display = "none"; // when game starts users are not allowed to visit the high scores page
+  timeDisplay.textContent = "Time: " + timer; // display timer
+  scoreCounter.textContent = "Score: " + score; //display score
+  // initialize timer
   const bigTimer = setInterval(function () {
+    // timer will decrement by 1 as long as the time left is greater than 0
     if (timer > 0) {
       timeDisplay.textContent = "Time: " + timer;
       timer = timer - 1;
+      // else the game is deemed over and the final score page will be rendered, clear the timer
     } else {
       timeDisplay.textContent = "Time: " + timer;
       renderFinalScore();
       clearInterval(bigTimer);
     }
   }, 1000);
+
+  // assigning event listeners to all buttons
   assignEventListener();
-  welcomePage.style.display = "none";
-  questionArray[0].style.display = "block";
+  welcomePage.style.display = "none"; // hide welcome page
+  questionArray[0].style.display = "block"; // render first question
 }
 
+// assigning event listeners to all buttons
 function assignEventListener() {
   for (let i = 0; i < buttonArray.length; i++) {
     buttonArray[i].addEventListener("click", renderQuestion);
   }
 }
 
+// disable all buttons when a question is answered, untill next question comes up
 function disableButtons(id) {
   let buttonArray = document.querySelectorAll(id);
 
@@ -62,19 +84,26 @@ function disableButtons(id) {
   }
 }
 
+// enable all buttons at the start of the next game
 function enableButtons() {
   for (let i = 0; i < buttonArray.length; i++) {
     buttonArray[i].disabled = false;
   }
 }
 
+// checking if the user selected the correct answer
 function checkAnswer(index, rightAnswer) {
+  // if this is not the last question
   if (index != 5) {
+    // if the answer selected is correct
     if (rightAnswer === "true") {
-      score = score + 2;
+      score = score + 2; // add 2 points to the score
+      scoreCounter.textContent = "Score: " + score;
+      // display correct or incorrect message
       resultMessage.style.display = "block";
       resultMessage.style.color = "Green";
       resultMessage.textContent = "Correct!";
+      // if the timer still has more than 1 second left, render the next question (no point of rendering the next question when the timer has less than 1s)
       if (timer > 1) {
         const transitionCorrct = setInterval(function () {
           resultMessage.style.display = "none";
@@ -83,17 +112,22 @@ function checkAnswer(index, rightAnswer) {
           clearInterval(transitionCorrct);
         }, 1000);
       }
+      // if the answer is wrong
     } else {
       if (timer >= 5) {
+        // minus 5 seconds from the timer as a punishment if the timer still has >= 5s left
         timer = timer - 5;
       } else {
         timer = 0;
       }
       if (score > 0) {
-        score = score - 1;
+        score = score - 1; // take away 1 point
+        scoreCounter.textContent = "Score: " + score;
       } else {
         score = 0;
+        scoreCounter.textContent = "Score: " + score;
       }
+      // display message of result
       resultMessage.style.display = "block";
       resultMessage.style.color = "red";
       resultMessage.textContent = "Incorrect!";
@@ -106,15 +140,17 @@ function checkAnswer(index, rightAnswer) {
         }, 1000);
       }
     }
+    // if this is the last question
   } else {
     if (rightAnswer === "true") {
       score = score + 2;
+      scoreCounter.textContent = "Score: " + score;
       resultMessage.style.display = "block";
       resultMessage.style.color = "Green";
       resultMessage.textContent = "Correct!";
       if (timer > 1) {
         const transitionCorrct = setInterval(function () {
-          timer = 0;
+          timer = 0; // set timer to 0 even if there's still time left.
           clearInterval(transitionCorrct);
         }, 1000);
       }
@@ -126,8 +162,10 @@ function checkAnswer(index, rightAnswer) {
       }
       if (score > 0) {
         score = score - 1;
+        scoreCounter.textContent = "Score: " + score;
       } else {
         score = 0;
+        scoreCounter.textContent = "Score: " + score;
       }
       resultMessage.style.display = "block";
       resultMessage.style.color = "red";
@@ -146,18 +184,20 @@ function checkAnswer(index, rightAnswer) {
   }
 }
 
+// the fucntion that will be triggered when buttons are clicked
 function renderQuestion(event) {
-  let id = event.target.getAttribute("class");
-  let rightAnswer = event.target.dataset.correct;
-  let index = 0;
+  let id = event.target.getAttribute("class"); // get the class attribute for the target button
+  let rightAnswer = event.target.dataset.correct; // this variable indicates whether or not the answer is correct.
+  let index = 0; // index that will decides which next question to render.
 
   event.preventDefault();
   event.stopPropagation();
 
   if (id.includes("q1")) {
+    // if the target button class includes 'q1'
     index = 0;
-    disableButtons(".q1");
-    checkAnswer(index, rightAnswer);
+    disableButtons(".q1"); // disable all button for the question.
+    checkAnswer(index, rightAnswer); // execute checkAnswer()
   } else if (id.includes("q2")) {
     index = 1;
     disableButtons(".q2");
@@ -171,6 +211,7 @@ function renderQuestion(event) {
     disableButtons(".q4");
     checkAnswer(index, rightAnswer);
   } else if (id.includes("q5")) {
+    index = 4;
     disableButtons(".q5");
     checkAnswer(index, rightAnswer);
   } else if (id.includes("q6")) {
@@ -184,22 +225,28 @@ function renderQuestion(event) {
   }
 }
 
+// function rendering the final score page
 function renderFinalScore() {
-  viewHighScoresButton.style.display = "none";
+  viewHighScoresButton.style.display = "none"; // won't allow users to check high scores yet
 
-  scoreListItem.scoreArray.push(score);
+  scoreListItem.scoreArray.push(score); // push the final score into the array.
 
   for (let i = 0; i < questionArray.length; i++) {
+    //make sure to hide all questions
     questionArray[i].style.display = "none";
   }
 
-  scoreDisplay.textContent = "Your score is: " + score;
+  scoreDisplay.textContent = "Your score is: " + score; // display score
+
+  score = 0; // reset score to 0
+  scoreCounter.textContent = "Score: " + score;
 
   resultMessage.style.display = "none";
 
   finalScorePage.style.display = "block";
 }
 
+// function for rendering a list of highest scores
 function renderHighScores() {
   for (let i = 0; i < questionArray.length; i++) {
     questionArray[i].style.display = "none";
@@ -211,23 +258,25 @@ function renderHighScores() {
 
   viewHighScoresButton.style.display = "none";
 
+  // get info from the local storage
   let listItem = JSON.parse(localStorage.getItem("scoreListItem"));
-  console.log(listItem);
   const ul = document.querySelector(".view-scores-list");
+  document.querySelector(".view-scores-list").textContent = ""; // clear all previously rendered list items, to prevent redundancy.
   for (let i = 0; i < listItem.scoreArray.length; i++) {
-    let li = document.createElement("li");
+    let li = document.createElement("li"); // create <li> element
     li.textContent =
       "User " +
       listItem.userInitials[i] +
       " scores " +
       listItem.scoreArray[i] +
-      "points!";
-    ul.appendChild(li);
+      " points."; // set text content
+    ul.appendChild(li); // append under ul
   }
 }
 
-startButton.addEventListener("click", init);
+startButton.addEventListener("click", init); // add event listener to the start game button
 
+// submitting final score and user's initials
 submitScoreButton.addEventListener("click", function (event) {
   event.preventDefault();
   event.stopPropagation();
@@ -238,16 +287,16 @@ submitScoreButton.addEventListener("click", function (event) {
     scoreListItem.userInitials.push(initials);
     nameInput.value = "";
   }
-  localStorage.setItem("scoreListItem", JSON.stringify(scoreListItem));
+  localStorage.setItem("scoreListItem", JSON.stringify(scoreListItem)); // store the info into local storage
 
   renderHighScores();
-  console.log(scoreListItem);
 });
 
-viewHighScoresButton.addEventListener("click", renderHighScores);
+viewHighScoresButton.addEventListener("click", renderHighScores); // renders the high scores page when clicked
 
+// the 'go back' button that will take users back to the game, start a new game.
 backToWelcomePageButton.addEventListener("click", function (event) {
-  timer = 5;
+  timer = 50;
 
   event.stopPropagation();
 
@@ -255,4 +304,12 @@ backToWelcomePageButton.addEventListener("click", function (event) {
   viewHighScoresPage.style.display = "none";
   welcomePage.style.display = "block";
   viewHighScoresButton.style.display = "block";
+});
+
+// clears all score history
+clearScoresButton.addEventListener("click", function () {
+  localStorage.removeItem("scoreListItem");
+  scoreListItem.scoreArray = [];
+  scoreListItem.userInitials = [];
+  document.querySelector(".view-scores-list").textContent = "";
 });
